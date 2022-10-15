208311c6faa1bfcea579e16e104e33483b0909f4
Miriam Andrés Martín <miriandres.dev@gmail.com>
2022-10-15 16:43:24 +0200

    remove form properties

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,7 +1,6 @@
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,38 +14,19 @@ export class DashboardComponent implements OnInit {
   @Input() img     : string = 'src';
   @Input() message : string = 'none';
 
-  loginForm : FormGroup;
 
-  get nameField (): FormControl {
-		return this.loginForm.get( 'name' ) as FormControl;
-	}
-	get lastNameField (): FormControl {
-		return this.loginForm.get( 'lastName' ) as FormControl;
-	}
+  // @Input() info : Info = {
+  //   title   : '',
+  //   message : '',
+  //   img     : ''
+  // }
 
   constructor(
     private router   : Router,
     private location : Location,
-    private fb       : FormBuilder
-  ) { 
-    this.loginForm = this.fb.group({
-      name: new FormControl('', Validators.required),
-      lastName: new FormControl('', Validators.required)
-    })
-  }
+  ) {}
 
   ngOnInit(): void {
-  }
-  
-  // User validation
-  onSubmit(form: any): void{
-    if(form.name == 'John' && form.lastName=='Doe'){
-      this.router.navigate(['checkout/step-2-payment']);
-    }
-    this.resetForm();
-  }
-  resetForm(): void{
-    this.loginForm.reset();
   }
 
   goBack(): void{
